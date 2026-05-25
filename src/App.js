e34a78f8a053ca0888741bf423b8fdf1ba1a39ed
kittiann-9737 <kittiann@gmail.com>
2026-05-25 18:56:39 +0700

    Fresh Start - My First Web App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 My First Web App</h1>
+      <p>สร้างมาจาก React + Vercel</p>
+      
+      <button onClick={() => alert('Hello World!')}>
+        คลิกฉัน
+      </button>
+    </div>
+  );
+}
+
+export default App;
