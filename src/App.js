7a445636b56bcce7c9fbe8407519d1390f8658c6
kittiann-9737 <kittiann@gmail.com>
2026-05-26 21:01:45 +0700

    modify app.js2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>🚀 My First Web App</h1>
-      <p>สร้างมาจากกก React + Vercel</p>
+      <p>สร้างมาจากกกdddd React + Vercel</p>
       
       <button onClick={() => alert('Hello World!')}>
         คลิกฉัน
